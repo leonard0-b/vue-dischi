bb3df10c26ede00634d4e64a3b9c8d533ec7efcf
Leonardo <leonardo.bongianni@gmail.com>
2021-04-09 15:00:57 +0200

    Ciclati album dall'array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 var app = new Vue({
   el: '#root',
   data: {
+    albums: []
   },
+  mounted() {
+    axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((response) => {
+          console.log(response.data.response);
+          this.albums = response.data.response
+          console.log(this.albums);
+        })
+  },
+
 });
